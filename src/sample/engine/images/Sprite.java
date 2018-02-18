4f615bd94945e81c6e2e5c75d86e62f5d906e8eb
Ciprian Khlud <ciprian.mustiata@yahoo.com>
2018-02-18 21:56:08 +0200

    More updates

--- src/sample/engine/images/Sprite.java
+++
@@ -1,7 +0,0 @@
-package sample.engine.images;
-
-public class Sprite extends Surface {
-    public Sprite(int width, int height) {
-        super(width, height);
-    }
-}
--- src/sample/engine/painter/GameEvent.java
+++
@@ -1,6 +0,0 @@
-package sample.engine.painter;
-
-public class GameEvent {
-    EventKind eventKind;
-    //public int EventKind;
-}
